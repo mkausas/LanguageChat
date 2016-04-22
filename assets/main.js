2a235d9f72ee56a5cb0bfe90502acacadacc4a75
mkausas <mkausas@gmail.com>
2016-04-21 23:36:41 -0400

    Update readme

--- assets/main.js
+++ assets/main.js
@@ -235,7 +235,7 @@ $(function() {
   socket.on('login', function (data) {
     connected = true;
     // Display the welcome message
-    var message = "Welcome to Socket.IO Chat – ";
+    var message = "Welcome to Language Chat – ";
     log(message, {
       prepend: true
     });
